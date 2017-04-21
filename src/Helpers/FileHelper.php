123a3b5d67ec5199f3bf7c27b03949c0eb9b7de9
s.marusza <s.marusza@gb.ad.corp>
2017-04-21 10:29:00 +0200

    pierwszy commit test

---
+++ src/Helpers/FileHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CIS\Helpers;
+
+class FileHelper {
+
+    static public function deleteAll($str) {
+        //It it's a file.
+        if (is_file($str)) {
+            //Attempt to delete it.
+            return unlink($str);
+        }
+        //If it's a directory.
+        elseif (is_dir($str)) {
+            //Get a list of the files in this directory.
+            $scan = glob(rtrim($str, '/') . '/*');
+            //Loop through the list of files.
+            foreach ($scan as $index => $path) {
+                //Call our recursive function.
+                self::deleteAll($path);
+            }
+            //Remove the directory itself.
+            return @rmdir($str);
+        }
+    }
+
+}
